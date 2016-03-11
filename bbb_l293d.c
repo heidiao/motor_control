f985c088e1b63b86936a31f4d7fb469c75d35191
Heidi Ao <heidiao126@me.com>
2016-03-11 20:24:33 +0800

    modify to array struct call

--- bbb_l293d.c
+++ bbb_l293d.c
@@ -2,63 +2,31 @@
 #include <linux/version.h>
 #include <linux/init.h>
 #include <linux/gpio.h>
-#include <linux/interrupt.h>
-#include <linux/gpio_keys.h>
 
-
-/*
-
-    int gpio_is_valid(int number);
-    int gpio_direction_input(unsigned gpio);
-    int gpio_direction_output(unsigned gpio, int value);
-
-    // GPIO INPUT:  return zero or nonzero 
-    int gpio_get_value(unsigned gpio);
-
-    // GPIO OUTPUT 
-    void gpio_set_value(unsigned gpio, int value);
-
-    // release previously-claimed GPIO 
-    void gpio_free(unsigned gpio);
-
-struct mgpio {
-    unsigned    gpio;
-    unsigned long   flags;
-    const char  *label;
-};
-
-static struct mgpio motor_gpios[] = {
+static struct gpio bbb_gpios[] = {
     { 48, GPIOF_OUT_INIT_LOW,  "Motor P9_15" }, // default to OFF 
     { 51, GPIOF_OUT_INIT_LOW,  "Motor P9_16"   }, // default to OFF 
 };
-*/
-
 
 static int __init bbb_l293d_init(void)
 {
     int err;
-//    err = gpio_request_array(motor_gpios, ARRAY_SIZE(motor_gpios));
- //   if (err)
-  //      gpio_free_array(motor_gpios, ARRAY_SIZE(motor_gpios));
-    err = gpio_request(48, "P9_15");
-    err = gpio_request(51, "P9_16");
-    gpio_direction_output(48,0);
-    gpio_direction_output(51,0);
-    err=gpio_export(48, true);
-    err=gpio_export(51, true);
+    err = gpio_request_array(bbb_gpios, ARRAY_SIZE(bbb_gpios));
+    if (err){
+        gpio_free_array(bbb_gpios, ARRAY_SIZE(bbb_gpios));
+        return false;
+    }
+    gpio_export(bbb_gpios[0].gpio, true);
+    gpio_export(bbb_gpios[1].gpio, true);
 
     return true;
-   // return i2c_add_driver(&mxt_driver);
 }
 
 static void __exit bbb_l293d_exit(void)
 {
-    gpio_unexport(48);
-    gpio_unexport(51);
-    gpio_free(48);
-    gpio_free(51);
-//    gpio_free_array(motor_gpios, ARRAY_SIZE(motor_gpios));
-//    i2c_del_driver(&mxt_driver);
+    gpio_unexport(bbb_gpios[0].gpio);
+    gpio_unexport(bbb_gpios[1].gpio);
+    gpio_free_array(bbb_gpios, ARRAY_SIZE(bbb_gpios));
 }
 
 module_init(bbb_l293d_init);
